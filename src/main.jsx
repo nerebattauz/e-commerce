543d1ceadb6be76a8d11101bd80dee05684f1225
Nerea Battauz <nereebattauz@gmail.com>
2025-02-25 00:07:32 -0300

    cambio de navbar segun context

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './components/navbar.jsx'
+import Navbar from './components/Navbar.jsx'
 import Footer from './components/footer.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+    <UserProvider>
     <ChakraProvider>
-    <Navbar />
     <App />
-    <Footer />
     </ChakraProvider>
+    </UserProvider>
     </BrowserRouter>
 )
